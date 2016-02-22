70b9f77850a5549eb8663e71ac1d7849b0e20bfa
daniel <daniel.leszczynski@redexperts.net>
2016-02-22 17:39:31 +0100

    scripts js

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+/**
+ * Created by daniel on 22.02.16.
+ */
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 80 + 'px'
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$('.navbar').addClass('original').clone().insertAfter('.navbar').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+function stickIt() {
+
+    var orgElementPos = $('.original').offset();
+    orgElementTop = orgElementPos.top;
+
+    if ($(window).scrollTop() >= (orgElementTop)) {
+        // scrolled past the original position; now only show the cloned, sticky element.
+
+        // Cloned element should always have same left position and width as original element.
+        orgElement = $('.original');
+        coordsOrgElement = orgElement.offset();
+        leftOrgElement = coordsOrgElement.left;
+        widthOrgElement = orgElement.css('width');
+        $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+        $('.original').css('visibility','hidden');
+    } else {
+        // not scrolled past the menu; only show the original menu.
+        $('.cloned').hide();
+        $('.original').css('visibility','visible');
+    }
+}
+
+$(document).ready(function() {
+    $('nav a').click(function () {
+        $('nav a').parent().removeClass('active');
+        $(this).parent().addClass('active');
+    });
+});
