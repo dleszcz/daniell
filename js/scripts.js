1fdeb54a773e629e809fe00b03133eda5686cb38
daniel <danielxleszczynski@gmail.com>
2016-02-23 19:25:43 +0100

    fix scroll & menu

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,48 @@ $(function() {
     });
 });
 
-$('.navbar').addClass('original').clone().insertAfter('.navbar').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
-scrollIntervalID = setInterval(stickIt, 10);
-
-function stickIt() {
-
-    var orgElementPos = $('.original').offset();
-    orgElementTop = orgElementPos.top;
-
-    if ($(window).scrollTop() >= (orgElementTop)) {
-        // scrolled past the original position; now only show the cloned, sticky element.
-
-        // Cloned element should always have same left position and width as original element.
-        orgElement = $('.original');
-        coordsOrgElement = orgElement.offset();
-        leftOrgElement = coordsOrgElement.left;
-        widthOrgElement = orgElement.css('width');
-        $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-        $('.original').css('visibility','hidden');
-    } else {
-        // not scrolled past the menu; only show the original menu.
-        $('.cloned').hide();
-        $('.original').css('visibility','visible');
-    }
-}
-
 $(document).ready(function() {
     $('nav a').click(function () {
         $('nav a').parent().removeClass('active');
         $(this).parent().addClass('active');
     });
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2 -80+"px"
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop() + 85;
+    $('.nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if (refElement.position().top <= scrollPos  && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav ul li').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            currLink.parent().removeClass("active");
+        }
+    });
+}
