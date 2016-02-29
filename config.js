949abbe37f67fd21cf7e9386d3abbba0c94cbb40
dljs <danielxleszczynski@gmail.com>
2016-02-29 20:31:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var mail = {};
 
 exports.mail = {
-    name: '******',
-    password: '******',
-    from: '******',
-    to: '******',
-    subject: '******',
-    service: '******',
+    name: 'daniell.wiadomosci@gmail.com',
+    password: 'Xdaniell!38',
+    from: '"daniell.pl" <daniell.wiadomosci@gmail.com>',
+    to: 'danielxleszczynski@gmail.com',
+    subject: 'Wiadomość z daniell.pl',
+    service: 'Gmail'
 };
